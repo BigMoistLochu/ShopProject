611ce0d8f4123ad7fd81332dc8e200a95a802334
BigMoistLochu <krok.konrad1@gmail.com>
2023-04-24 21:50:37 +0200

    change filterProducts and add this feature to the Scheduler

--- src/main/java/com/example/projektpowtorzeniowy/ProjektPowtorzeniowyApplication.java
+++ src/main/java/com/example/projektpowtorzeniowy/ProjektPowtorzeniowyApplication.java
@@ -4,15 +4,19 @@ import com.example.projektpowtorzeniowy.apiPublic.ApiProviderData;
 import com.example.projektpowtorzeniowy.mappers.MapperProvider;
 import com.example.projektpowtorzeniowy.model.Product;
 import com.example.projektpowtorzeniowy.repository.RepositoryProvider;
+import com.example.projektpowtorzeniowy.taskScheduler.FilterProductTask;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.List;
-@Slf4j
+
 @SpringBootApplication
+@EnableScheduling
+@Slf4j
 public class ProjektPowtorzeniowyApplication implements CommandLineRunner {
 
 
@@ -41,38 +45,6 @@ public class ProjektPowtorzeniowyApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args){
-
-
-        //Proste sprawdzenie czy obiektow ktore przyszly z publicznego api sa juz w bazie danych(jesli sa to ich nie dodaje, jesli nie ma to dodaje)
-
-        List<Product> listOFProductBeforeFilter =  apiProviderData.getProductsFromPublicApi().stream()
-                .map(productDto -> mapperProvider.getProductMapper().mapper(productDto))
-                .toList();
-
-        List<Product> listOfProductFiltered = listOFProductBeforeFilter.stream()
-                .filter(product -> {
-                    try {
-                       return !product.getTitle().equals(repositoryProvider.getProductRepository().getFirstByTitle(product.getTitle()).getTitle());
-                    }
-                    catch(NullPointerException e)
-                    {
-                        log.info("Cos Poszlo Nie tak ");
-                        return true;
-                    }
-                })
-                .toList();
-
-        addFilteredProductToDataBase(listOfProductFiltered);
-
-
-
-    }
-
-
-
-    public void addFilteredProductToDataBase(List<Product> productListAfterFiltered)
-    {
-        productListAfterFiltered.stream().map(product -> repositoryProvider.getProductRepository()
-                        .save(product)).toList();
+        log.info("Start Aplikacji");
     }
 }
---
+++ src/main/java/com/example/projektpowtorzeniowy/taskScheduler/FilterProductTask.java
@@ -0,0 +1,53 @@
+package com.example.projektpowtorzeniowy.taskScheduler;
+
+import com.example.projektpowtorzeniowy.apiPublic.ApiProviderData;
+import com.example.projektpowtorzeniowy.mappers.MapperProvider;
+import com.example.projektpowtorzeniowy.model.Product;
+import com.example.projektpowtorzeniowy.repository.RepositoryProvider;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Slf4j
+@AllArgsConstructor
+public class FilterProductTask {
+
+
+    ApiProviderData apiProviderData;
+
+    RepositoryProvider repositoryProvider;
+
+
+    MapperProvider mapperProvider;
+
+
+
+    @Scheduled(cron = "*/10 * * * * *")
+    public void FilterProductBeforeAddToDataBase()
+    {
+        //funkcja zaciagajac produkty z publicznego api sprawdza czy taki element
+        //nie zostal juz wczesniej dodany, jesli tak to nie dodaje w przeciwnym wypadku dodaje
+        //testowo ustawione na 10sekund
+        apiProviderData.getProductsFromPublicApi().stream()
+                .map(productDto -> mapperProvider.getProductMapper().mapper(productDto))
+                .filter(product -> {
+                    try {
+                        return !product.getTitle().equals(repositoryProvider.getProductRepository().getFirstByTitle(product.getTitle()).getTitle());
+                    }
+                    catch(NullPointerException e)
+                    {
+                        log.info("Cos Poszlo Nie tak ");
+                        return true;
+                    }
+                }).map(product -> repositoryProvider.getProductRepository()
+                        .save(product)).toList();
+    }
+
+
+
+}
